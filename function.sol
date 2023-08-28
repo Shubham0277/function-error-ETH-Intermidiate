615ed6a983d5f1ca1a12578dd7ddb6f54900f935
Shubham0277 <141708942+Shubham0277@users.noreply.github.com>
2023-08-28 21:22:57 +0530

    Update function.sol

--- function.sol
+++ function.sol
@@ -1,28 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract functionsanderror {
-    uint256 public nums;
-
-    constructor(uint256 _initialValue) {
-        nums = _initialValue;
+contract CustomErrorHandling {
+    function checkGreaterThanZero(uint256 number) public pure returns (uint256) {
+        // Check if the number is greater than zero
+        require(number > 0, "Number must be greater than zero");
+        return number * 4;
     }
 
-    function updateValue(uint256 _newnum) external {
-        require(_newnum > 0, "New Number  must be more than zero");
-        nums = _newnum;
+    function validateNotZero(uint256 number) public pure returns (uint256) {
+        // Validate that the number is not zero
+        assert(number != 0);
+        return number + 2;
     }
 
-    function getCurrentValue() external view returns (uint256) {
-        return nums;
-    }
-
-    function removeAmount(uint256 _amount) external {
-        require(_amount <= nums, "Amount has been crossed the limit ");
-        nums -= _amount;
-
-        if (nums == 0) {
-            revert("Number has been reached upto the zero");
+    function checkNotAllowed(uint256 number) public pure returns (uint256) {
+        // Check if the number is not 7
+        if (number == 7) {
+            revert("7 is not allowed");
         }
+        return number - 1;
+    }
+
+    function intentionallyTriggerErrors() public pure {
+        // Function to intentionally trigger the error handlers
+
+        // Trigger the require error by passing a negative number
+        checkGreaterThanZero(0);
+
+        // Trigger the assert error by passing zero
+        validateNotZero(0);
+
+        // Trigger the revert error by passing 88
+        checkNotAllowed(88);
     }
 }
